cd034e29eca561b23b32605284c677dd219124e4
David <dvddcrz@gmail.com>
2024-07-23 08:23:05 +0100

    fix ecosystem

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,6 @@
-import purgecss from '@fullhuman/postcss-purgecss'
-import autoprefixer from 'autoprefixer'
-import cssnano from 'cssnano'
-import tailwindcss from 'tailwindcss'
-
-const purgeCSSConfig = purgecss({
-  content: ['./public/**/*.html', './src/**/*.vue', './src/**/*.jsx', './src/**/*.js'],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  safelist: [
-    'body', // add the body class to the safelist
-    /^Minimal3x5$/ // ensure your custom font family class is not purged
-  ]
-})
-
-const plugins = [
-  tailwindcss,
-  autoprefixer,
-  // Add CSSNano for CSS minification
-  cssnano({
-    preset: 'default'
-  })
-]
-
-// eslint-disable-next-line no-undef
-if (process.env.VITE_NETWORK === 'mainnet') {
-  plugins.push(purgeCSSConfig)
-}
-
 export default {
-  plugins
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 }
